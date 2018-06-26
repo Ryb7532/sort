2a5612d9381c44e9d483e637b717df57177ac17c
Ryuhei Mori <mori@c.titech.ac.jp>
2018-06-26 11:36:11 +0900

    revised

---
+++ src/quick_sort_improved.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//フェルマー素数
+#define N ((1<<16)+1)
+
+int A[N];
+
+/*
+A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
+*/
+void quick_sort(int A[], int n){
+}
+
+int main(){
+  int i;
+  A[0] = 0;
+  A[1] = 3; //原始元
+  for(i=2;i<N;i++){
+    A[i] = (long long int) A[i-1] * A[1] % N;
+  }
+
+  quick_sort(A, N);
+  for(i=0;i<N;i++){
+    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+  }
+}
