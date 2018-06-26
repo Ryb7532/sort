c748acf8eeefbfe4329cc13084bb69efcc904e7d
Ryubu Hosoki <hosoki.r.aa@m.titech.ac.jp>
2018-06-26 16:13:23 +0900

    finish

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -9,6 +9,22 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    int i, j, pivot, z;
+    pivot = A[0];
+    if(n <= 1) return;
+    for(i = j = 1; i < n; i++){
+        if(A[i] <= pivot){
+            z = A[j];
+            A[j] = A[i];
+            A[i] = z;
+            j++;
+        }
+    }
+    A[0] = A[j-1];
+    A[j-1] = pivot;
+    quick_sort(A, j);//pivot以下
+    quick_sort(A+j, n-j);//pivotより大きい
+    return;
 }
 
 int main(){
